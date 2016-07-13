a81f013c2d9433f3cfd11067ac3428daee620b97
Lance Fallon <lancefallon@Lances-MBP.fios-router.home>
2016-07-12 20:13:42 -0400

    foundation test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 var App = React.createClass({
   render: function(){
     return (
-      <h2>App.jsx</h2>
+      <div className="callout primary">App.jsx</div>
     );
   }
 });
